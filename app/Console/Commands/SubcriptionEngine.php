ada71c3e404354188d3684b2d99f9f65fafe154b
Daniel Makinde <daniel@blinqpay.io>
2021-02-27 09:23:21 +0100

    Add name, signature and description of the command

--- app/Console/Commands/SubcriptionEngine.php
+++ app/Console/Commands/SubcriptionEngine.php
@@ -12,14 +12,14 @@ class SubcriptionEngine extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'redis:subscribe';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Subscribe to a channel';
 
     /**
      * Create a new command instance.
