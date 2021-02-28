32733cc29db90d344243d25e1ff14036ce5830ec
Daniel Makinde <daniel@blinqpay.io>
2021-02-28 23:06:53 +0100

    Configure api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PubSubController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post(
+    '/publish/{TOPIC}',
+    [PubSubController::class, 'publish']
+);
